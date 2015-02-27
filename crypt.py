11228f1c792bd3e39a8fefaeffe0cf6bea5b35ae
Strauji <ezequiasrpg@gmail.com>
2015-02-26 23:41:22 -0300

    26/02/15 update

--- crypt.py
+++ crypt.py
@@ -1,26 +1,30 @@
-
-
 from ucrypt import execute
+
 try_again = 'y'
+
+################################################################
 print 'uCrypt 2.0'
 print 'Please keep in mind that some keys may not work as well'
 print 'So, we have a "debug" system.'
 print '[-] = Encrypted string'
 print '[+] = Output with same key'
 print ''
+################################################################
+#############################################################################
 while (try_again == 'y'):
 	msg = raw_input('Enter what you want to be encrypted:')
 	key = raw_input('Enter a key:')
 	print 'It may take several seconds until get finished, please wait'
 
 	
-	a = execute(key,msg,False)
+	a = execute(key,msg)
 	
-	print  '[-] => ' + a
+	print  "[-] => " + a
 
 	print  ' ------------- '
-	b = execute(key,a,False)
+	b = execute(key,a)
 	
-	print  '[+] => ' + b
+	print  "[+] => " + b
 	try_again = raw_input('Want to try again?[y/n]:')
+############################################################################
 print 'Done'
--- ucrypt.py
+++ ucrypt.py
@@ -1,5 +1,6 @@
+#######################################################
 def fibonacci(pos):
-    if not type(pos) == int:
+    if not type(pos) == int: 
         return 'Error - Key must be an integer'
     else:
         n = 1
@@ -13,17 +14,14 @@ def fibonacci(pos):
 	  nn = n
 	  n = nn+nnn
         return n
-                
-def execute(key, msg,is_plus):
-   
-    if type(key) == int:
-        return simple(key,msg,is_plus)
-	
-		
-    elif type(key) == str:
-       # return complex(key,msg,is_plus) - Disabled for a while
+#######################################################  
+#############################################################            
+def execute(key, msg):
+       return simple(key,msg) #While it can't parser "n:x:y"-like keys, it just jump to "simple"
        print 'Error - uCrypt cant parser string keys yet...'
-    
+#############################################################    
+'''
+####Disabled for a while
 def complex(key,msg,plus):
     if not type(key) == str:
         return 'Error - complex key must be an string'
@@ -37,28 +35,22 @@ def complex(key,msg,plus):
 
           f = simple(ke,f,plus)
         return f
-def simple(key,msg,plus):
+'''
+###########################################################
+def simple(key,msg):
     key = int(key)
     if not type(key) == int: 
-	#just to avoid (IM)POSSIBLES errors
+	#yep, i know that it is redundant.
         return 'Error - simple key must be an integer'
     else:
-	r = ''
-	 
+	r = '' #initialize r	 
         for i in msg:
-             
-
-
             rest = fibonacci(key)
             rest = rest % 256
 	    ascii_char = ord(i)
             c =  ascii_char - rest
-	  
-	  
-	   
-	   
 	    r = r + chr(abs(c))    
         return r
-
+###########################################################
     
-
+#Only God know why it works...
